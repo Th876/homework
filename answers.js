a3dda7675f6579f97266d87251de23c6355775f0
TH876 <tayhiggins14@gmail.com>
2023-03-15 09:54:05 -0400

    Get Even answered

--- answers.js
+++ answers.js
@@ -1,7 +1,7 @@
 //Arrays, Loops, and Conditional Reps
 
 ////////////////////////////////
-// Easy Going
+// Easy Going answered
 //Write a for loop that will log the numbers 1 through 20.
 ////////////////////////////////
 
@@ -10,16 +10,32 @@ for(let i = 1; i < 21; i++) {
 }
 
 
+////////////////////////////////
+// Get Even answered
+//Write a for loop that will log only the even numbers in 0 through 200.
+//Hint: Think about the increment expression.
+////////////////////////////////
+for(let i = 0; i < 201; i+=2) {
+    console.log(i);
+}
 
-////////////////////////////////
-// Get Even
-////////////////////////////////
 
 
 ////////////////////////////////
 // Fizz Buzz
+/*Write a javascript application that logs all numbers from 1 - 100.
+If a number is divisible by 3 log "Fizz" instead of the number.
+If a number is divisible by 5 log "Buzz" instead of the number.
+If a number is divisible by 3 and 5 log "FizzBuzz" instead of the number.*/
 ////////////////////////////////
 
+function fizzBuzz(num) {
+    for(let i = 1; i < 101; i++) {
+        if(i % 3 === 0 && i % 5 === 0) {
+            
+        }
+    }
+}
 
 ////////////////////////////////
 // Wild Wild Life
